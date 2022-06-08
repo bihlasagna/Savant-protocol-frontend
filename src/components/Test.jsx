eaf712ebca60fea8267890f9778a1c873d890e0d
devmanchine <brightsolution4@outlook.com>
2022-06-08 09:52:41 -0400

    Add tailwindcss configuration

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="test">
+      <p className="uppercase">This is test</p>
+    </div>
+  );
+};
+
+export default Test;
