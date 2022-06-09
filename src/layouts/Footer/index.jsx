7da6282e3cf9cf44f6600a485dc18ef7d0a5dd06
devmanchine <brightsolution4@outlook.com>
2022-06-09 16:13:49 -0400

    Add footer component

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -1,5 +1,15 @@
+import './index.scss';
+
 const Footer = () => {
-  return <h1>This is a Footer</h1>;
+  return (
+    <footer className="flex flex-row items-center">
+      <img src="assets/images/logo.png" alt="Savant Logo" />
+      <div className="content">
+        <div className="title font-bold text-5xl">SAVANT</div>
+        <div className="desc">@ 2022 SAVANT, INC</div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
