de2dc0e7d1eaf4f723842467454aafa147bffb34
devmanchine <brightsolution4@outlook.com>
2022-06-11 19:34:24 -0400

    Add resources section in the landing page

---
+++ src/components/Resources/index.jsx
@@ -0,0 +1,16 @@
+import './index.scss';
+
+const Resources = () => {
+  return (
+    <div className="resources flex flex-col justify-center items-center">
+      <div className="resources-container flex flex-col justify-center items-center">
+        <div className="resources-title text-white">
+          RESOURCES TO GET STARTED
+        </div>
+        <img src="./assets/images/flow.png" alt="Resource" />
+      </div>
+    </div>
+  );
+};
+
+export default Resources;
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,14 @@
 // import Button from 'components/Button';
 import Overview from 'components/Overview';
+import Resources from 'components/Resources';
+
 const HomePage = () => {
-  return <Overview />;
+  return (
+    <>
+      <Overview />
+      <Resources />
+    </>
+  );
 };
 
 export default HomePage;
