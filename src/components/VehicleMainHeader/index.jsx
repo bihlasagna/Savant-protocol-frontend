c4fe06afe8eca9aa64e125aab00a76693f0475d9
devmanchine <brightsolution4@outlook.com>
2022-06-13 15:44:13 -0400

    Add todo for social icons

--- src/components/VehicleMainHeader/index.jsx
+++ src/components/VehicleMainHeader/index.jsx
@@ -1,14 +1,23 @@
-import cx from 'classnames';
-import './index.scss';
 import EstateImage from 'components/EstateImage';
 import EstateMainHeaderContent from 'components/EstateMainHeaderContent';
+import './index.scss';
 
+/**
+ * TODO: Add social icons
+ */
 const VehicleMainHeader = () => {
   return (
-    <div className="vehicle-main-header flex flex-row text-white">
-      <EstateImage />
-      <EstateMainHeaderContent />
-    </div>
+    <>
+      <div className="vehicle-main-header">
+        <div className="vehicle-main-container flex flex-row text-white">
+          <EstateImage />
+          <EstateMainHeaderContent />
+        </div>
+        <div className="social-icons text-white flex flex-row justify-end">
+          Here
+        </div>
+      </div>
+    </>
   );
 };
 
