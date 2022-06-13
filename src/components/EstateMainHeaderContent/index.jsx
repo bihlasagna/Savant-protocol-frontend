4ec1a02a38067950215904405b9d606b6964586a
devmanchine <brightsolution4@outlook.com>
2022-06-13 14:50:59 -0400

    Add vehicle main content component

---
+++ src/components/EstateMainHeaderContent/index.jsx
@@ -0,0 +1,23 @@
+import './index.scss';
+
+const EstateMainHeaderContent = () => {
+  return (
+    <div className="estate-main-header-content">
+      <p className="estate-main-header-title">15 SUMMER STREET</p>
+      <div className="estate-main-header-content flex flex-row">
+        <p className="desc w-1/2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis
+          tortor vitae sapien lacinia porttitor. Etiam non dignissim dui. Etiam
+          metus tortor, commodo nec congue venenatis, ornare sit amet leo.
+          Phasellus sed mauris elit. Pellentesque sed pharetra augue.
+          Pellentesque sodales molestie purus, in malesuada neque elementum nec.
+          Interdum et malesuada fames ac ante ipsum primis in faucibus. In
+          laoreet tortor quis neque malesuada, eu semper nisi venenatis.
+        </p>
+        <div className="details"></div>
+      </div>
+    </div>
+  );
+};
+
+export default EstateMainHeaderContent;
--- src/components/VehicleMainHeader/index.jsx
+++ src/components/VehicleMainHeader/index.jsx
@@ -1,11 +1,13 @@
 import cx from 'classnames';
 import './index.scss';
 import EstateImage from 'components/EstateImage';
+import EstateMainHeaderContent from 'components/EstateMainHeaderContent';
 
 const VehicleMainHeader = () => {
   return (
-    <div className="vehicle-main-header flex flex-row">
+    <div className="vehicle-main-header flex flex-row text-white">
       <EstateImage />
+      <EstateMainHeaderContent />
     </div>
   );
 };
