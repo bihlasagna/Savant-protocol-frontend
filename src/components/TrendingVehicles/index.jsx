af1f2f17f2d306d192d9db13d4fb49bc440be5e3
devmanchine <brightsolution4@outlook.com>
2022-06-13 02:38:51 -0400

    Add trending vehicle list component

--- src/components/TrendingVehicles/index.jsx
+++ src/components/TrendingVehicles/index.jsx
@@ -1,4 +1,29 @@
+import Vehicle from 'components/Vehicle';
 import './index.scss';
+
+const trendingVehicles = [
+  {
+    title: 'NY COLLECTIVE',
+    stablePrice: '$34.66',
+    total: 'Ξ 1,456.2',
+    apy: '+56.2%',
+    avatar: 'venture1',
+  },
+  {
+    title: '20 BROAD',
+    stablePrice: '$2.67',
+    total: 'Ξ 356.1',
+    apy: '+9.8%',
+    avatar: 'venture2',
+  },
+  {
+    title: 'GVS ABNB',
+    stablePrice: '$12.85',
+    total: 'Ξ 1,253.6',
+    apy: '+14.9%',
+    avatar: 'venture3',
+  },
+];
 
 const TrendingVehicles = () => {
   return (
@@ -6,7 +31,11 @@ const TrendingVehicles = () => {
       <div className="title text-5xl font-bold text-white text-center">
         TRENDING VEHICLES
       </div>
-      <div className="vehicle-list"></div>
+      <div className="vehicle-list flex flex-col flex-wrap">
+        {trendingVehicles.map((vehicle, index) => (
+          <Vehicle index={index + 1} vehicle={vehicle} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Vehicle/index.jsx
+++ src/components/Vehicle/index.jsx
@@ -1,10 +1,22 @@
 import cx from 'classnames';
 import './index.scss';
 
-const Vehicle = ({ name }) => {
+const Vehicle = ({ vehicle, index }) => {
+  const { title, avatar, total, apy, stablePrice } = vehicle;
   return (
-    <div className="vehicle-item">
-      <img src={`./assets/images/${name}.png`} alt="Vehicle" />
+    <div className="vehicle flex flex-row items-center">
+      <p className="vehicle-number text-white">{index}</p>
+      <div className="vehicle-content flex flex-row items-center">
+        <img src={`./assets/images/${avatar}.svg`} alt="Vehicle" />
+        <div className="content">
+          <p className="vehicle-title text-white">{title}</p>
+          <p className="vehicle-stable text-white">{stablePrice}</p>
+        </div>
+      </div>
+      <div className="vehicle-detail">
+        <p className="vehicle-total text-white">{total}</p>
+        <p className="vehicle-apy text-primary">{apy}</p>
+      </div>
     </div>
   );
 };
